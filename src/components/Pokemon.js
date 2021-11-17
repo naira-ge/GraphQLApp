eb47d19231b682faf9e0b48a74adf6560765e1a6
Naira <nairagezhoyan@gmail.com>
2021-11-17 22:48:11 +0400

    add sass

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -1,35 +1,34 @@
 import React from 'react';
+import styles from './styles.module.scss';
 
 function Pokemon ( pokemon ){
     return (
-        <div className="pokemon">
-            <div className="pokemon__name">
+        <div className={ styles.pokemon }>
+            <div className={ styles.pokemon__name }>
                 <p>{pokemon.pokemon.name}</p>
             </div>
-            <div className="pokemon__meta">
+            <div className={ styles.pokemon__meta }>
                 <span>{ pokemon.pokemon.maxHP }</span>
                 <span>{pokemon.pokemon.maxCP}</span>
             </div>
-            <div className="pokemon__image">
+            <div className={ styles.pokemon__image }>
                 <img src={pokemon.pokemon.image} alt={pokemon.pokemon.name}/>
             </div>
-            <div className="pokemon__attacks">
-                <span>
+            <div className={ styles.pokemon__attacks }>
                     <p>Special Attacks: </p>
                     { pokemon?.pokemon?.attacks?.special?.slice( 0, 3 ).map( attack => (
                         <span key={`${attack.name}-${attack.damage}`}>
                             {attack.name}
                         </span>
                     ))}
-                </span>
-                <span>
+            </div>
+            <div className={ styles.pokemon__attacks }>
                     <p>Fast Attacks: </p>
                     { pokemon?.pokemon?.attacks?.fast?.slice( 0, 3 ).map( attack => (
                         <span key={`${attack.name}-${attack.damage}`}>
                             {attack.name}
                         </span>
                     ))}
-                </span>
             </div>
         </div>
     )
